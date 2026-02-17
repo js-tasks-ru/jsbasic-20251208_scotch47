940594762173040c0b522f406d0f08baef187ea1
Vitaliy Ratnikov <vitaly.ratnikov747@gmail.com>
2026-02-17 15:05:13 +0100

    module 8 - task 1-4

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,29 +6,55 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return;
+    }
+
+    let cartItem = this.cartItems.find(
+      (item) => item.product.id === product.id,
+    );
+
+    if (cartItem) {
+      cartItem.count += 1;
+    } else {
+      cartItem = { product, count: 1 };
+      this.cartItems.push(cartItem);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.find((item) => item.product.id === productId);
+    if (!cartItem) {
+      return;
+    }
+
+    cartItem.count += amount;
+
+    if (cartItem.count <= 0) {
+      this.cartItems = this.cartItems.filter((item) => item !== cartItem);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((sum, item) => sum + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce(
+      (sum, item) => sum + item.product.price * item.count,
+      0,
+    );
   }
 
   onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
-
